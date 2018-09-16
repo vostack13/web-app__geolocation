ac7448ad93d84868a4ddac457d7155b40f56b96e
Stanislav Vozikov <stanislav.vozikov@gmail.com>
2018-09-16 21:17:27 +0300

    update: обновление макета балуна

--- src/components/georeviews/view.js
+++ src/components/georeviews/view.js
@@ -24,15 +24,78 @@ export default {
 
     renderMarks(objMap = {}, propertiesMarks = []) {
         // Создание макета содержимого балуна.
-        const balloonLayout = ymaps.templateLayoutFactory.createClass(
+        // const balloonLayoutTemp = ymaps.templateLayoutFactory.createClass( 
+            // '',
+            
+        //     {
+        //         build: function () {
+        //             balloonLayoutTemp.superclass.build.call(this);
+
+        //             const userName = document.getElementById('userName')
+        //             const commentButtonAdd = document.getElementById('commentButtonAdd')
+        //             const balloon = document.querySelector('.balloon')
+
+        //             commentButtonAdd.addEventListener('click', this.onCounterClick)
+        //         },
+
+        //         clear: function () {
+        //             commentButtonAdd.removeEventListener('click', this.onCounterClick)
+        //             balloonLayoutTemp.superclass.clear.call(this);
+        //         },
+
+        //         onSublayoutSizeChange: function () {
+        //             balloonLayoutTemp.superclass.onSublayoutSizeChange.apply(this, arguments);
+
+        //             if(!this._isElement(this.balloon)) {
+        //                 return;
+        //             }
+
+        //             this.applyElementOffset();
+
+        //             this.events.fire('shapechange');
+        //         },
+
+        //         applyElementOffset: function () {
+        //             this.balloon.css({
+        //                 left: -(this.balloon[0].offsetWidth / 2),
+        //                 top: -(this.balloon[0].offsetHeight + this.balloon.find('.arrow')[0].offsetHeight)
+        //             });
+        //         },
+
+        //         getShape: function () {
+        //             if(!this._isElement(this.balloon)) {
+        //                 return balloonLayoutTemp.superclass.getShape.call(this);
+        //             }
+
+        //             var position = this.balloon.position();
+
+        //             return new ymaps.shape.Rectangle(new ymaps.geometry.pixel.Rectangle([
+        //                 [position.left, position.top], [
+        //                     position.left + this.balloon[0].offsetWidth,
+        //                     position.top + this.balloon[0].offsetHeight + this.balloon.find('.arrow')[0].offsetHeight
+        //                 ]
+        //             ]));
+        //         },
+
+        //         onCounterClick: function () {
+        //             // console.log('Внутри шаблона маркера: ' + userName.value)
+        //             emitEventEmitter('addComment', userName.value)
+        //         },
+        //     }
+        // )
+
+        
+        const balloonLayoutContent = ymaps.templateLayoutFactory.createClass(
             '<div class="balloon">' +
-                '<header> {{ properties.baloonInfo.address }} </header>' +
+                '<header> <h2> {{ properties.baloonInfo.address }} </h2> <a id="close" href="#">&times;</a> </header>' +
                 '<main>' +
                     '<ul id="commentsList">' +
                         '{% for user in properties.baloonInfo.users %}' +
                             '<li>' + 
-                                '<b>{{ user.name }}</b>' +
-                                '<p>{{ user.comments }}</p>' +
+                                '<h4 class="comment__name">{{ user.name }} </h4>' +
+                                '<span class="comment__place">{{ user.place }} </span>' +
+                                '<span class="comment__date">{{ user.date }}</span>' +
+                                '<p class="comment__text">{{ user.comments }}</p>' +
                             '</li>' +
                         '{% endfor %}' +
                     '</ul>' +
@@ -43,33 +106,85 @@ export default {
                     '<button id="commentButtonAdd">Добавить</button>' +
                 '</main>' +
             '</div>', 
+
             {
                 build: function () {
-                    balloonLayout.superclass.build.call(this);
+                    this.constructor.superclass.build.call(this);
 
                     const userName = document.getElementById('userName')
                     const commentButtonAdd = document.getElementById('commentButtonAdd')
+                    const closeButton = document.getElementById('close')
+                    const balloon = document.querySelector('.balloon')
 
                     commentButtonAdd.addEventListener('click', this.onCounterClick)
+                    closeButton.addEventListener('click', this.onCloseClick.bind(this))
                 },
 
                 clear: function () {
                     commentButtonAdd.removeEventListener('click', this.onCounterClick)
-                    balloonLayout.superclass.clear.call(this);
+                    this.constructor.superclass.clear.call(this);
                 },
 
                 onCounterClick: function () {
-                    // console.log('Внутри шаблона маркера: ' + userName.value)
                     emitEventEmitter('addComment', userName.value)
-                }
+                },
+
+                onCloseClick: function (e) {
+                    e.preventDefault();
+
+                    this.events.fire('userclose');
+                },
+
+                // onSublayoutSizeChange: function () {
+                //     balloonLayoutTemp.superclass.onSublayoutSizeChange.apply(this, arguments);
+
+                //     if(!this._isElement(this.balloon)) {
+                //         return;
+                //     }
+
+                //     this.applyElementOffset();
+
+                //     this.events.fire('shapechange');
+                // },
+
+                // applyElementOffset: function () {
+                //     this.balloon.css({
+                //         left: -(this.balloon[0].offsetWidth / 2),
+                //         top: -(this.balloon[0].offsetHeight + this.balloon.find('.arrow')[0].offsetHeight)
+                //     });
+                // },
+
+                // getShape: function () {
+                //     if(!this._isElement(this.balloon)) {
+                //         return balloonLayoutTemp.superclass.getShape.call(this);
+                //     }
+
+                //     var position = this.balloon.position();
+
+                //     return new ymaps.shape.Rectangle(new ymaps.geometry.pixel.Rectangle([
+                //         [position.left, position.top], [
+                //             position.left + this.balloon[0].offsetWidth,
+                //             position.top + this.balloon[0].offsetHeight + this.balloon.find('.arrow')[0].offsetHeight
+                //         ]
+                //     ]));
+                // },
             }
         )
+
+
         const placemarks = propertiesMarks.map(mark => {
             return new ymaps.Placemark(mark.coord, {
-                baloonInfo: mark
+                baloonInfo: mark,
             }, {
                 present: 'islands#blueHomeCircleIcon',
-                balloonContentLayout: balloonLayout
+                // balloonLayout: balloonLayoutTemp,
+                balloonContentLayout: balloonLayoutContent,
+                balloonPanelMaxMapArea: 0,
+                hideIconOnBalloonOpen: false,
+                balloonOffset: [3, -40],
+                balloonMaxWidth: 380,
+                balloonMaxHeight: 528,
+                balloonCloseButton: false
             })
         })
 
